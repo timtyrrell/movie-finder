17980e9244f752d9b53e122d62c2dc2c988ce84a
Tim Tyrrell <tim@tyrrell.io>
2021-02-20 16:52:19 -0700

    chore: initial repo cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from './test-utils';
+import {screen, render} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
+  const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
