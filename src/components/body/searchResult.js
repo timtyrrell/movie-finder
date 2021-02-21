272d0c94a4d9893c53fa9aebb5bcc6f76ca5da34
Tim Tyrrell <tim@tyrrell.io>
2021-02-21 15:05:43 -0700

    feat: strip out html in summary and handle lack of images in response

--- src/components/body/searchResult.js
+++ src/components/body/searchResult.js
@@ -2,11 +2,12 @@ import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
 
 export const SearchResult = ({ movie }) => {
   const { name, image, summary } = movie.show;
+  const sanitizedSummary = summary.replace(/<[^>]+>/g, "");
   return (
     <Box>
-      <Image src={image.medium}></Image>
+      <Image src={image?.medium}></Image>
       <Heading>{name}</Heading>
-      <Text>{summary}</Text>
+      <Text>{sanitizedSummary}</Text>
       <Button>Show episodes</Button>
     </Box>
   );
