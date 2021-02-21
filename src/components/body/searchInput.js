1483672f66204157ebc9b7076e471bff1398cf4f
Tim Tyrrell <tim@tyrrell.io>
2021-02-21 11:43:48 -0700

    feat: add in input box and search icon

--- src/components/body/searchInput.js
+++ src/components/body/searchInput.js
@@ -1,9 +1,22 @@
 import {
-  Text,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement
 } from '@chakra-ui/react';
+import {SearchIcon} from '@chakra-ui/icons'
 
 export const SearchInput = () => {
   return (
-    <Text>Input</Text>
+    <>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon/>}
+        />
+        <Input placeholder="search show titles" />
+      </InputGroup>
+      <Button>Search</Button>
+    </>
   )
 }
